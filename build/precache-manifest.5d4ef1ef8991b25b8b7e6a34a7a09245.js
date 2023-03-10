13edf7b463cbdecd5365343ce2819e4c5d9a26d7
Priyanka Chaurasia <pc24priyanka@gmail.com>
2023-03-10 13:56:17 +0530

    build

---
+++ build/precache-manifest.5d4ef1ef8991b25b8b7e6a34a7a09245.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8209c53da1c9b0c657aa65fa0faaae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1b2979ca1f1af0f550e",
+    "url": "/static/js/2.ff1f862f.chunk.js"
+  },
+  {
+    "revision": "49fcedd183a9818f19cf",
+    "url": "/static/js/main.d6c21fc2.chunk.js"
+  },
+  {
+    "revision": "7c7f2061c905b0de73f4",
+    "url": "/static/js/runtime-main.04feb0a8.js"
+  }
+]);
